ed6c428af9eb31610ba8b1f1b7a0bcb4c86c31ba
Lisa Hagin <lhagin@encompass.tv>
2017-02-17 11:06:33 -0500

    finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,66 @@
-var guessCount = 0;
+
 var wins = 0;
 var losses = 0;
-var chances = 3;
+var MAXTRIES = 3;
 var rLetter = "";
+var attempts = MAXTRIES;
+var DEBUG = 0;
 
 
 //picks a random letter between a and z
 function randomLetter() {
+    var fuzzy =  Math.floor(Math.random() * 3) +1;
+    console.log(fuzzy);
     var chars = "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQURSTUVWXYZ";
     rLetter= chars.substr( Math.floor(Math.random() * 52), 1);
-    document.getElementById("hint").innerHTML="Hint: " +rLetter;
+    if (DEBUG == 1){
+      console.log(chars.indexOf(rLetter));
+      console.log(rLetter);
+    }
+    
+    document.getElementById("hint").innerHTML="It's near: " +chars[chars.indexOf(rLetter) - fuzzy];
 } 
 
 function guessOne(){
- 
   // Get a guess from the player
   var playerGuess = document.getElementById("guess").value;
-  document.getElementById("wins").innerHTML= "Total Wins: " +wins;
-  document.getElementById("losses").innerHTML= "Total Losses: " +losses;
-  document.getElementById("chances").innerHTML= "Chances Left: " +chances;
 
    if (playerGuess == rLetter){
      wins+=1;
-     document.getElementById("message").innerHTML= "Correct!";    
+     document.getElementById("message").innerHTML= "Correct! Keep Guessing.";    
      document.getElementById("wins").innerHTML= "Total Wins: " +wins;
      randomLetter();
   } else {
     losses+=1;
-    if(losses==3){
-      chances-=1;
+    if(attempts==1){   
       document.getElementById("guessbtn").disabled=true;
       document.getElementById("message").innerHTML= "Sorry Game Over! Hit Restart Button";
     } else{
-      document.getElementById("message").innerHTML= "Nope! You have " + chances + " left.";
+      attempts-=1;
       
+      document.getElementById("message").innerHTML= "Nope! You have " + attempts + " left.";    
     }
-    document.getElementById("losses").innerHTML= "Total Losses: " +losses;
   }
+  document.getElementById("wins").innerHTML= "Total Wins: " +wins;
+      document.getElementById("losses").innerHTML= "Total Losses: " +losses;
+      if (DEBUG == 1){
+        console.log(attempts);
+      }
 }
+
 document.onsubmit = function(e){
-e.preventDefault();
+  e.preventDefault();
 }
+
+//reset game
 function newGame(){
+  // losses=0;
+  attempts=MAXTRIES;
+  if (DEBUG == 1){
+    console.log(attempts);
+  }
+  document.getElementById("wins").innerHTML= "";
+  document.getElementById("losses").innerHTML= "";
   document.getElementById("guessbtn").disabled=false;
   randomLetter();
   document.getElementById("message").innerHTML= "Good Luck!";
